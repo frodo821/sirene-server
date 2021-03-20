94b52ed045e6e70a7870e2049ac145d868b96aa6
frodo821 <sakaic2003@gmail.com>
2021-03-20 20:30:19 +0900

    リポジトリ層に移動

--- app/repositories/initialization.py
+++ app/repositories/initialization.py
@@ -1,7 +1,7 @@
 from os.path import dirname, join
 from yaml import safe_load
 from app.infra.player import MidiPlayer
-from app.infra.fileLookup import FileLookup
+from app.infra.file_lookup import FileLookup
 from atexit import register
 
 with open(join(dirname(__file__), 'config.yaml')) as f:
