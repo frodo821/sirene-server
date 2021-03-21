0edded5f8928b5d285a67f15dc8bcb43e3f8d125
frodo821 <sakaic2003@gmail.com>
2021-03-21 10:18:49 +0900

    メソッドを実装

--- app/domains/server/server_repository_interface.py
+++ app/domains/server/server_repository_interface.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+from app.domains.server.server import ServerStatus
 
 
 class ServerRepositoryInterface:
   @abstractmethod
-  def get_current_status(self):
+  def get_current_status(self) -> ServerStatus:
     pass
--- app/repositories/server.py
+++ app/repositories/server.py
@@ -1,6 +1,7 @@
 from app.domains.server.server_repository_interface import ServerRepositoryInterface
 from app.infra.file_lookup import FileLookup
 from app.infra.player import MidiPlayer
+from app.domains.server.server import ServerStatus
 
 
 class ServerRepository(ServerRepositoryInterface):
@@ -9,4 +10,8 @@ class ServerRepository(ServerRepositoryInterface):
     self.lookup = lookup
 
   def get_current_status(self):
-    self.player
+    return ServerStatus(
+        playing=bool(self.player.music) and not self.player.paused,
+        connectedPorts=[{"port": conn.connector.port} for conn in self.player.connectors],
+        playingMusic=self.player.music.json() if self.player.music else None
+    )
