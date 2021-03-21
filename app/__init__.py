6e1a412cbb2e17bc51c88fb9a721fd5247f42915
frodo821 <sakaic2003@gmail.com>
2021-03-21 10:17:43 +0900

    終了ハンドラを追加

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-import app.repositories.initialization  # noqa
+from app.repositories.initialization import player
 from app.routers import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+
+@app.on_event("shutdown")
+def shutdown():
+  player.close()
--- app/repositories/initialization.py
+++ app/repositories/initialization.py
@@ -2,15 +2,9 @@ from os.path import dirname, join
 from yaml import safe_load
 from app.infra.player import MidiPlayer
 from app.infra.file_lookup import FileLookup
-from atexit import register
 
-with open(join(dirname(__file__), 'config.yaml')) as f:
-  config: dict = safe_load(f)
+with open(join(dirname(__file__), '..', 'config.yaml')) as f:
+  config: dict = safe_load(f) or {}
 
 player = MidiPlayer(128)
-lookup = FileLookup(config.get('dir', join(dirname(__file__), '..', 'midis')))
-
-
-@register
-def close():
-  player.close()
+lookup = FileLookup(config.get('dir', join(dirname(__file__), '..', '..', 'midis')))
