a00b6a4902db9b73ebeedc24e9424193e356bf16
frodo821 <sakaic2003@gmail.com>
2021-03-21 10:20:14 +0900

    ルーターを実装

--- app/routers/status.py
+++ app/routers/status.py
@@ -1,8 +1,14 @@
 from fastapi.routing import APIRouter
+from app.usecases.get_server_status import GetServerStatus
+from app.repositories.server import ServerRepository, ServerStatus
+from app.repositories.initialization import player, lookup
 
 router = APIRouter(prefix='/status')
 
 
-@router.get('')
-def get_status(self):
-  pass
+@router.get('', response_model=ServerStatus)
+def get_status():
+  status = GetServerStatus(
+      ServerRepository(player, lookup)
+  ).execute()
+  return dict(status)
