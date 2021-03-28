480934747c063b6b587a0d5ee6a6a1db4f038ac8
frodo821 <sakaic2003@gmail.com>
2021-03-28 18:03:49 +0900

    実験的機能の有効無効を切り替えられるように変更

--- app/repositories/initialization.py
+++ app/repositories/initialization.py
@@ -6,7 +6,7 @@ from app.infra.file_lookup import FileLookup
 
 __all__ = ['frontend_loader', 'player', 'lookup']
 
-config_path = join(dirname(__file__), '..', 'config.yaml')
+config_path = join(dirname(__file__), '..', '..', 'config.yaml')
 
 if not exists(config_path):
   with open(config_path, 'w', encoding='utf-8') as f:
@@ -16,7 +16,7 @@ if not exists(config_path):
                 'base_path': './frontend/public',
             },
             'time_resolution': 128,
-            'midi_dir': './midis'
+            'midi_dir': './midis',
         },
         stream=f,
         default_flow_style=False,
@@ -27,7 +27,12 @@ with open(config_path) as f:
   config: dict = safe_load(f) or {}
 
 frontend: dict = config.get('frontend', {})
+experimentals: dict = config.get('experimentals', {})
 
 frontend_loader = FrontendLoader(frontend.get('base_path'))
-player = MidiPlayer(config.get('time_resolution', 128))
+
+player = MidiPlayer(
+    config.get('time_resolution', 128),
+    use_experimental_arduino_driver=experimentals.get('next_gen_arduino_driver', False))
+
 lookup = FileLookup(config.get('midi_dir', './midis'))
