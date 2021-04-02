523278bb70815e8bb439e43242b5ceb7b81eba22
frodo821 <sakaic2003@gmail.com>
2021-04-02 11:56:44 +0900

    WebSocketにデータを送信する間隔を1フレームに短縮

--- app/routers/websocket.py
+++ app/routers/websocket.py
@@ -3,9 +3,10 @@ from fastapi import APIRouter
 from threading import Thread
 from asyncio import sleep
 
-from app.repositories.initialization import player
+from app.repositories.initialization import player, config
 
 router = APIRouter()
+wait = 1 / config.get('time_resolution', 128)
 
 
 @router.websocket('/ws')
@@ -14,11 +15,10 @@ async def connect_websocket(ws: WebSocket):
 
   try:
     while True:
-      if player.music and not player.paused:
-        await ws.send_json({
-            "notes": player.playing_notes,
-            "time": player.playback_time
-        })
-      await sleep(0.1)
+      await ws.send_json({
+          "notes": player.playing_notes,
+          "time": player.playback_time
+      })
+      await sleep(wait)
   except:
     await ws.close()
