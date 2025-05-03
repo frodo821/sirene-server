844e3c3d49553dcdc211d3af6413d08f02182586
frodo821 <sakaic2003@gmail.com>
2025-05-03 16:54:27 +0900

    実行スクリプトを追加

--- app/exec.py
+++ app/exec.py
@@ -4,9 +4,8 @@ from app import app
 
 def main():
   run(
-      app,
-      port=8000,
-      host="0.0.0.0",
-      # log_level="warning",
-      access_log=False,
+    app,
+    port=8000,
+    host="0.0.0.0",
+    # log_level="warning",
   )
